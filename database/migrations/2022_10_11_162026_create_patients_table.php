23ad7d0aa00b4ee433927a2c43d61043f7a8a67e
mariojimenez04 <97321877+mariojimenez04@users.noreply.github.com>
2025-01-24 17:02:03 -0600

    Actualizacion BD

--- database/migrations/2022_10_11_162026_create_patients_table.php
+++ database/migrations/2022_10_11_162026_create_patients_table.php
@@ -36,6 +36,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('registro_numero_series');
+        Schema::dropIfExists('patients');
     }
 };
